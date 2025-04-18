67776ed68bb478e4ff70737fe63000197ad20f72
marwa mannai <80351966+marwa21esprit@users.noreply.github.com>
2025-04-18 19:09:20 +0100

    Update TrainingSessionService.java

--- src/main/java/com/example/pi/service/trainingSessionServices/TrainingSessionService.java
+++ src/main/java/com/example/pi/service/trainingSessionServices/TrainingSessionService.java
@@ -1,15 +1,11 @@
 package com.example.pi.service.trainingSessionServices;
 
-import com.example.pi.config.HmsConfig;
-import com.example.pi.entity.Booking;
+
 import com.example.pi.entity.TrainingSession;
 import com.example.pi.entity.UserInfo;
 import com.example.pi.interfaces.trainingSession.ITrainingSessionService;
 import com.example.pi.repository.UserInfoRepository;
-import com.example.pi.repository.trainignSessionRepo.BookingRepository;
 import com.example.pi.repository.trainignSessionRepo.TrainingSessionRepository;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -17,7 +13,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 @Service
 @RequiredArgsConstructor
@@ -26,11 +21,8 @@ public class TrainingSessionService implements ITrainingSessionService {
     private final TrainingSessionRepository trainingSessionRepository;
     @Autowired
     private final UserInfoRepository userInfoRepository;
-    @Autowired
-    private final HmsService hmsService;
-    @Autowired
-    private final BookingRepository bookingRepository;
-    private final HmsConfig hmsConfig;
+
+
 
     @Override
     public TrainingSession createSession(TrainingSession trainingSession) {
@@ -45,11 +37,7 @@ public class TrainingSessionService implements ITrainingSessionService {
 
         // naffectiwah l trainingsession
         trainingSession.setCoach(coach);
-      /*  String roomId = hmsService.createRoom(trainingSession);
-        System.out.println(roomId);
-        String meetLink = hmsService.generateMeetingLink(roomId);
-        trainingSession.setHmsRoomId(roomId);
-        trainingSession.setHmsRoomLink(meetLink); */
+
         if (trainingSession.getStartTime().isAfter(trainingSession.getEndTime())) {
             throw new RuntimeException("Start time must be before end time");
         }
@@ -61,26 +49,8 @@ public class TrainingSessionService implements ITrainingSessionService {
     @Override
     public TrainingSession updateSession(Long id, TrainingSession trainingSession) {
         // Check if the session exists
-        TrainingSession existingSession = trainingSessionRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Training session not found with id: " + id));
-
-        // Update the fields of the existing session
-        if(!id.equals(existingSession.getId())) { return existingSession; }
-        existingSession.setId(id);
-        existingSession.setDate(trainingSession.getDate());
-        existingSession.setDescription(trainingSession.getDescription());
-        existingSession.setStartTime(trainingSession.getStartTime());
-        existingSession.setEndTime(trainingSession.getEndTime());
-        existingSession.setMaxParticipants(trainingSession.getMaxParticipants());
-        existingSession.setSport(trainingSession.getSport());
-        existingSession.setCoach(trainingSession.getCoach());
-        existingSession.setReviews(trainingSession.getReviews());
-        existingSession.setExercices(trainingSession.getExercices());
-        existingSession.setBookings(trainingSession.getBookings());
-        // Add other fields to update as needed
-
-        // Save the updated session
-        return trainingSessionRepository.save(existingSession);
+        trainingSession.setId(id);
+        return trainingSessionRepository.save(trainingSession);
     }
 
     @Override
